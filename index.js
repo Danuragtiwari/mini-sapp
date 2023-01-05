f3f952cbe0a010e12fb413b83d4f5086e8a3c74e
Dauragtiwari <www.anuragtiwari23@gmail.com>
2023-01-05 16:28:57 +0530

    adding icons and various required items

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var audio = new Audio('assets/sentmessage.mp3');
-var contactString = "<div class='social'> <a href='mailto:shikha.py36@gmail.com'> <div class='socialItem'><img class='socialItemI' src='images/gmail.svg' alt=''></div> </a> <a target='_blank' href='https://github.com/Shikha-code36'> <div class='socialItem'><img class='socialItemI' src='images/github.svg' alt=''></div> </a> ><a href='https://www.linkedin.com/in/36-shikha-pandey/' target='_blank' rel='noopener noreferrer'> <div class='socialItem'><img class='socialItemI' src='images/linkedin.svg' alt=''></div> </a> </div>";
-var resumeString = "<img src='images/resumeThumbnail.png' class='resumeThumbnail'><div class='downloadSpace'><div class='pdfname'><img src='images/pdf.png'><label>Shikha Pandey Resume.pdf</label></div><a href='assets/ShikhaPandey-Resume.pdf' download='ShikhaPandey-Resume.pdf'><img class='download' src='images/downloadIcon.svg'></a></div>";
-var addressString = "<div class='mapview'><iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d238.63833262443757!2d74.19014864534314!3d16.865338763272877!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc1a7dcf40f5dd7%3A0xd7b69fe1fcfa9877!2zMTbCsDUxJzU1LjQiTiA3NMKwMTEnMjUuMyJF!5e0!3m2!1sen!2sin!4v1645079906766!5m2!1sen!2sin' class='map'></iframe></div><label class='add'><address>Noida, Uttar Pradesh, INDIA 110096</address>";
+var contactString = "<div class='social'> <a href='mailto:www.anuragtiwaritiwari@gmail.com'> <div class='socialItem'><img class='socialItemI' src='gmail.png' alt='width:50px;height:20px;'></div> </a> <a target='_blank' href='https://github.com/Danuragtiwari'> <div class='socialItem'><img class='socialItemI' src='github.png' alt=''></div> </a> <a href='https://www.linkedin.com/in/danurag23s/' target='_blank' rel='noopener noreferrer'> <div class='socialItem'><img class='socialItemI' src='linkedin.png' alt=''></div> </a> </div>";
+var resumeString = "<img src='' class='resumeThumbnail'><div class='downloadSpace'><div class='pdfname'><label>Anurag_resume.pdf</label></div><a href='Anurag_resume.pdf' download='Anurag_resume.pdf'><img class='download' src='download.png'></a></div>";
+var addressString = '<div class="mapview"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3447.549944728598!2d75.69747551459795!3d30.221390117147923!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3910f895f5532341%3A0x1ee822a09942ee50!2sBoys%20Hostel%20No.%2010%2C%20Campus%20Rd%2C%20Punjab%20148106!5e0!3m2!1sen!2sin!4v1672913036976!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div><label class="add"><address>Sangrur, Punjab, INDIA 14007</address>';
 
 function startFunction() {
     setLastSeen();
@@ -73,20 +73,20 @@ function waitAndResponce(inputText) {
     switch (inputText.toLowerCase().trim()) {
         case "intro":
             setTimeout(() => {
-                sendTextMessage("Hello there 👋🏻,<br><br>My name is <span class='bold'><a class='alink'>Shikha Pandey</a>.</span><br><br>I am a  <span class='bold'>Software Engineer</span><br><br>I am eager to hear about potential career opportunities, so I would be pleased to chat about job openings in the engineering sphere.<br><br>Send <span class='bold'>'help'</span> to know more about me.<br>");
+                sendTextMessage("Hello there 👋🏻,<br><br>My name is <span class='bold'><a class='alink'>Anurag Tiwari</a>.</span><br><br>I am a  <span class='bold'>PreFinal Student @ SLIET</span><br><br>I am eager to hear about potential career opportunities, so I would be pleased to chat about job openings in the Your Organisation.<br><br>Send <span class='bold'>'help'</span> to know more about me.<br>");
             }, 2000);
             break;
         case "help":
-            sendTextMessage("<span class='sk'>Send Keyword to get what you want to know about me...<br>e.g<br><span class='bold'>'skills'</span> - to know my skills<br><span class='bold'>'resume'</span> - to get my resume<br><span class='bold'>'education'</span> - to get my education details<br><span class='bold'>'address'</span> - to get my address<br><span class='bold'>'contact'</span> - to get ways to connect with me<br><span class='bold'>'projects'</span> - to get details of my projects<br><span class='bold'>'clear'</span> - to clear conversation<br><span class='bold'>'about'</span> - to know about this site</span>");
+            sendTextMessage("<span class='sk'>Send Keyword to get what you want to know about me...<br>e.g<br><span class='bold'>'skills'</span> - to know my skills<br><span class='bold'>'resume'</span> - to get my resume<br><span class='bold'>'education'</span> - to get my education details<br><span class='bold'>'address'</span> - to get my address<br><span class='bold'>'profiles'</span>-to get details of all my coding profiles<br><span class='bold'>'contact'</span> - to get ways to connect with me<br><span class='bold'>'projects'</span> - to get details of my projects<br><span class='bold'>'clear'</span> - to clear conversation<br><span class='bold'>'about'</span> - to know about this site</span>");
             break;
         case "resume":
             sendTextMessage(resumeString);
             break;
         case "skills":
-            sendTextMessage("<span class='sk'>I am currently working as a Software Engineer<br><br>I can comfortably write code in following languages :<br><span class='bold'>Python<br>C++<br>CSS<br>HTML<br>Javascript</span><br><br>I've experiance with following frameworks :<span class='bold'><br>Flask<br>Django<br>ReactJs<br>Bootstrap</span><br><br>I have experience with <br><span class='bold'>AWS services<br>Docker<br></span><br>Favourite IDE:<span class='bold'>VSCode</span></span>");
+            sendTextMessage("<span class='sk'>I am currently In PreFinal @SLIET <br><br>I  code in following languages :<br><span class='bold'>Python<br>C<br>CSS<br>HTML<br>Javascript</span><br><br>I've experiance with following frameworks :<span class='bold'><br>Django<br>Bootstrap</span><br><br>Favourite IDE:<span class='bold'>VSCode</span></span>");
             break;
         case "education":
-            sendTextMessage("I have completed my Btech from College Of Engineering Roorkee <br>Passing Year : 2020<br><br>I have completed my Secondary school from Modern Era Public School<br>Passing Year:2016");
+            sendTextMessage("I am Currently pursuing BTECH in ECE from @SLIET,Sangrur <br> Year : 2020-2024<br><br>I have completed my Secondary school from Narayana Junior College,Vizag<br>Passing Year:2020s <br><br>I have completed my High School from Carmel Convent School,Bishrampur<br>Passing Year:2018<br>");
             break;
 
         case "address":
@@ -96,16 +96,19 @@ function waitAndResponce(inputText) {
             clearChat();
             break;
         case "about":
-            sendTextMessage("🛠️💻 This portfolio website is built using HTML, CSS and JavaScript from SCRATCH!<br><br>👨🏻‍💻 Designed and Developed by <a class='alink' target='_blank' href='https://www.linkedin.com/in/36-shikha-pandey/'><span class='bold'>Shikha Pandey</a> with ❤️</span>");
+            sendTextMessage("🛠️💻 This portfolio website is built using HTML, CSS and JavaScript from SCRATCH!<br><br>👨🏻‍💻 Designed and Developed by <a class='alink' target='_blank' href='https://www.linkedin.com/in/danurag23'><span class='bold'>Anurag Tiwari</a> with ❤️</span>");
             break;
         case "contact":
             sendTextMessage(contactString);
             break;
         case "projects":
-            sendTextMessage("You want to check my projects? Then just jump into my Github Account.<br><br><div class='social'><a target='_blank' href='https://github.com/Shikha-code36'> <div class='socialItem'><img class='socialItemI' src='images/github.svg' alt=''></div> </a></div>");
+            sendTextMessage("You want to check my projects? Then just jump into my Github Account.<br><br><div class='social'><a target='_blank' href='https://github.com/Danuragtiwari> <div class='socialItem'><img class='socialItemI' src='github.png' alt=''></div> </a></div>");
             break;
         case "new":
             sendTextMessage(addressString);
+            break;
+        case "profiles":
+            sendTextMessage("You Can check my various coding profiles as below:<br><br><div class='social'><a target='_blank' href='https://github.com/Danuragtiwari'> <div class='socialItem'><img class='socialItemI' src='github.png' alt=''><label>Github</label></div> </a></div><br><br><div class='social'><a target='_blank' href='https://leetcode.com/Danurag/'> <div class='socialItem'><img class='socialItemI' src='leetcode.png' alt=''><label>LeetCode</label></div> </a></div><br><br><div class='social'><a target='_blank' href='https://auth.geeksforgeeks.org/user/danurag'> <div class='socialItem'><img class='socialItemI' src='gfg.jpg' alt=''><label>GeeksForGeeks</label></div> </a></div>")
             break;
         default:
             setTimeout(() => {
